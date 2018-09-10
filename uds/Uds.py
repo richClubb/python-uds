cf78b0247e7949ea558c22310c6bf685d2783e05
Richard Clubb <richardc@embeduk.com>
2018-09-10 12:51:05 +0100

    Added behaviour for reading the configuration file
    added pseudo semaphore flag into the send function to enable the use of
    tester present

--- uds/Uds.py
+++ uds/Uds.py
@@ -11,7 +11,7 @@ __status__ = "Development"
 
 
 from TpFactory import TpFactory
-
+from ConfigSingleton import get_config
 
 ##
 # @brief a description is needed
@@ -22,21 +22,43 @@ class Uds(object):
     # @param [in] reqId The request ID used by the UDS connection, defaults to None if not used
     # @param [in] resId The response Id used by the UDS connection, defaults to None if not used
     def __init__(self, reqId=None, resId=None):
-        self.__tp = TpFactory.tpFactory("CAN", reqId=reqId, resId=resId)
-        self.__reqId = reqId
-        self.__resId = resId
 
+        self.__config = get_config()
+
+        if reqId is None:
+            self.__config['connection']['defaultReqId']
+
+        if resId is None:
+            self.__config['connection']['defaultResId']
+
+        # currently the TP Factory only supports can
+        self.__tp = TpFactory.tpFactory("CAN", reqId=reqId, resId=resId)
+
+        self.__P2_CAN_Client = self.__config['connection']['P2_Client']
+        self.__P2_CAN_Server = self.__config['connection']['P2_Server']
+
+        # used as a semaphore for the tester present
+        self.__transmissionActive_flag = False
 
     ##
     # @brief
-    def send(self, msg, responseRequired=True):
+    def send(self, msg, responseRequired=True, functionalReq=False):
+
+        # sets a current transmission in progress
+        self.__transmissionActive_flag = True
 
         response = None
 
-        self.__tp.send(msg)
+        self.__tp.send(msg, functionalReq)
 
-        if(responseRequired):
+        if(
+                responseRequired |
+                functionalReq is False
+        ):
             response = self.__tp.recv(5000)
+
+        # lets go of the hold on transmissions
+        self.__transmissionActive_flag = False
 
         return response
 
