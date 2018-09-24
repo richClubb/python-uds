b51c209e2aa873ce52c0d261414b41b1d5a53d2b
Richard Clubb <rich.clubb@gmail.com>
2018-09-24 01:23:20 +0100

    Working commit

---
+++ uds_configuration/Config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+__author__ = "Richard Clubb"
+__copyrights__ = "Copyright 2018, the python-uds project"
+__credits__ = ["Richard Clubb"]
+
+__license__ = "MIT"
+__maintainer__ = "Richard Clubb"
+__email__ = "richard.clubb@embeduk.com"
+__status__ = "Development"
+
+
+from configparser import ConfigParser
+from os import path
+
+
+class Config(ConfigParser):
+
+    def __init__(self):
+        super(Config, self).__init__()
+
+
