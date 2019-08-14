fbca468b5ddd7b265ed0c28c8aecc5fd2fb900f4
Richard Clubb <rich.clubb@gmail.com>
2019-08-14 23:08:18 +0100

    Modified changelog and setup for release

---
+++ CHANGES.md
@@ -0,0 +1,10 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [1.1.0] - 2019-08-14
+### Added
+- Added socket can interface for linux support, tested with MCP2515 shield
+- Added UDS retry on response pending negative response code
+
+### Changed
+- Changed config path to make it OS agnostic for Linux support. Tested on Ubuntu 18.04 and Raspbian Stretch
--- setup.py
+++ setup.py
@@ -27,14 +27,14 @@ setup(
     # Needed for dependencies
     install_requires=['python-can>=3.0.0', 'python-lin>=0.1.0'],
     # *strongly* suggested for sharing
-    version='1.0.2',
+    version='1.1.0',
     # The license can be anything you like
     license='MIT',
     description='A library for interfacing with UDS using python',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
         "Operating System :: OS Independent"
     ],
     include_package_data=True
